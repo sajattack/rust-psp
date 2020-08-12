258e6977a151ccf85db7e6d4188e418133ec7017
Paul Sajna <sajattack@gmail.com>
2020-08-12 14:14:40 -0700

    fixup

--- psp/src/sys/vfpu.rs
+++ psp/src/sys/vfpu.rs
@@ -474,57 +474,69 @@ pub unsafe extern "C" fn sceVfpuColorSetRGB(
     
 #[no_mangle]
 pub unsafe extern "C" fn sceVfpuMemcpy(
-    mut dst: *mut u8,
-    mut src: *const u8,
+    mut dst8: *mut u8,
+    mut src8: *const u8,
     mut size: usize,
 ) -> *mut u8 {
     if size == 0 {
-        return dst
+        return dst8
     }
+    
 
-    while size > 63 {
-        vfpu_asm! {
-            lv_q C000, a1;
-            lv_q C010, 16(a1);
-            lv_q C020, 32(a1);
-            lv_q C030, 48(a1);
-            sv_q C000, 0(a0);
-            sv_q C010, 16(a0);
-            sv_q C020, 32(a0);
-            sv_q C030, 48(a0);
-            .mips "addiu a2, a2, -64";
-            .mips "addiu a1, a1, 64";
-            .mips "addiu a0, a0, 64";
-            : : "{4}"(dst), "{5}"(src), "{6}"(size) : "memory" : "volatile"
-        };
-    }
+    let mut dst32 = dst8 as *mut u32;
+    let mut src32 = src8 as *const u32;
 
-    while size > 15 {
-        vfpu_asm! {
-            lv_q C000, a1;
-            sv_q C000, a0;
-            .mips "addiu a2, a2, -16";
-            .mips "addiu a1, a1, 16";
-            .mips "addiu a0, a0, 16";
-            : : "{4}"(dst), "{5}"(src), "{6}"(size) : "memory" : "volatile"
+    if ((src8 as u32)&0xF) == 0 //Both src and dst are 16byte aligned
+    {
+        while size > 63 {
+            vfpu_asm! {
+                lv_q C000, 0(a1);
+                lv_q C010, 16(a1);
+                lv_q C020, 32(a1);
+                lv_q C030, 48(a1);
+                sv_q C000, 0(a0);
+                sv_q C010, 16(a0);
+                sv_q C020, 32(a0);
+                sv_q C030, 48(a0);
+                //.mips "addiu $$a2, $$a2, -64";
+                .mips "addiu $$a1, $$a1, 64";
+                .mips "addiu $$a0, $$a0, 64";
+                : : "{4}"(dst8), "{5}"(src8), "{6}"(size) : "memory" : "volatile"
+            };
+            size = size.saturating_sub(64);
         }
-    }
 
-    let mut dst32 = dst as *mut u32;
-    let mut src32 = src as *const u32;
+        while size > 15 {
+            vfpu_asm! {
+                lv_q C000, 0(a1);
+                sv_q C000, 0(a0);
+                //.mips "addiu $$a2, $$a2, -16";
+                .mips "addiu $$a1, $$a1, 16";
+                .mips "addiu $$a0, $$a0, 16";
+                : : "{4}"(dst8), "{5}"(src8), "{6}"(size) : "memory" : "volatile"
+            }
+            size = size.saturating_sub(16);
+        }
 
-    while size > 3 {
-        *dst32 = *src32;
-        dst32 = dst32.add(1);
-        src32 = src32.add(1);
-        size -= 1;
-    }
+        let mut dst32 = dst8 as *mut u32;
+        let mut src32 = src8 as *const u32;
 
-    while size > 0 {
-        *dst = *src;
-        dst.add(1);
-        src.add(1);
-        size -= 1;
+        while size > 3 {
+            *dst32 = *src32;
+            dst32 = dst32.add(1);
+            src32 = src32.add(1);
+            size = size.saturating_sub(4);
+        }
+
+        while size > 0 {
+            *dst8 = *src8;
+            dst8 = dst8.add(1);
+            src8 = src8.add(1);
+            size = size.saturating_sub(1);
+        }
+        dst8
+   
+    } else {
+         panic!("FUCK");
     }
-    dst
 }
