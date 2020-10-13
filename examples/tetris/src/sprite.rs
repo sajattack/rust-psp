44caaabf8bb51c95370bab6c65df74532bfc2390
Damian J Pound <damian@thebestisp.com>
2020-10-13 09:53:39 -0500

    rustfmt only to avoid polluting next commit

--- examples/tetris/src/sprite.rs
+++ examples/tetris/src/sprite.rs
@@ -1,10 +1,9 @@
-use core::{ptr, f32::consts::PI};
-use psp::Align16;
+use core::{f32::consts::PI, ptr};
 use psp::sys::{
-    self, ScePspFVector3,
-    GuPrimitive, MipmapLevel, VertexType,
-    GuSyncMode, GuSyncBehavior, GuContextType
+    self, GuContextType, GuPrimitive, GuSyncBehavior, GuSyncMode, MipmapLevel, ScePspFVector3,
+    VertexType,
 };
+use psp::Align16;
 
 #[repr(align(4))]
 pub struct Align4<T>(pub T);
@@ -22,7 +21,7 @@ pub struct Vertex {
 
 #[repr(C)]
 pub struct Sprite<'a, T> {
-    texture: &'a T, 
+    texture: &'a T,
     color: u32,
     x: i32,
     y: i32,
@@ -32,8 +31,11 @@ pub struct Sprite<'a, T> {
     scale: f32,
 }
 
-impl<'a, T> Sprite<'a, T> where T: AsRef<[u8]> {
-    pub const fn new(texture: &'a T, color: u32, x: i32, y: i32, width: u32, height: u32) -> Self { 
+impl<'a, T> Sprite<'a, T>
+where
+    T: AsRef<[u8]>,
+{
+    pub const fn new(texture: &'a T, color: u32, x: i32, y: i32, width: u32, height: u32) -> Self {
         Self {
             texture,
             color,
@@ -46,9 +48,9 @@ impl<'a, T> Sprite<'a, T> where T: AsRef<[u8]> {
         }
     }
 
-    pub fn as_vertices(&self) -> [Vertex; 2]   {
+    pub fn as_vertices(&self) -> [Vertex; 2] {
         [
-            Vertex { 
+            Vertex {
                 u: 0.0,
                 v: 0.0,
                 color: self.color,
@@ -63,41 +65,53 @@ impl<'a, T> Sprite<'a, T> where T: AsRef<[u8]> {
                 x: self.x as f32 + self.width as f32,
                 y: self.y as f32 + self.height as f32,
                 z: 0.0,
-            }
+            },
         ]
     }
 
     /// Don't use this if you're drawing many sprites, it's really slow
-    /// Use fn as_vertices() and collect your sprites into a single buffer to 
+    /// Use fn as_vertices() and collect your sprites into a single buffer to
     /// draw all at once. Note as_vertices() does not preserve scaling or rotation.
     pub fn draw(&self, displaylist: &mut Align16<[u32; 0x40000]>) {
         let vertices = Align16(self.as_vertices());
 
         unsafe {
             sys::sceGuStart(GuContextType::Direct, displaylist.0.as_mut_ptr() as *mut _);
-            
+
             sys::sceGumMatrixMode(sys::MatrixMode::Model);
             sys::sceGumLoadIdentity();
-            sys::sceGumScale(&ScePspFVector3 { x: self.scale, y: self.scale, z: 1.0 });
+            sys::sceGumScale(&ScePspFVector3 {
+                x: self.scale,
+                y: self.scale,
+                z: 1.0,
+            });
             //sys::sceGumTranslate(&ScePspFVector3 { x: self.x as f32, y: self.y as f32, z: 0.0});
             sys::sceGumRotateZ(self.rotation_radians);
             // setup texture
-            sys::sceGuTexImage(MipmapLevel::None, self.width as i32, self.height as i32, self.width as i32, self.texture.as_ref().as_ptr() as *const _); 
-            sys::sceGuTexScale(1.0/self.width as f32, 1.0/self.height as f32);
+            sys::sceGuTexImage(
+                MipmapLevel::None,
+                self.width as i32,
+                self.height as i32,
+                self.width as i32,
+                self.texture.as_ref().as_ptr() as *const _,
+            );
+            sys::sceGuTexScale(1.0 / self.width as f32, 1.0 / self.height as f32);
 
             sys::sceKernelDcacheWritebackInvalidateAll();
 
             // draw sprite
             sys::sceGumDrawArray(
                 GuPrimitive::Sprites,
-                VertexType::TEXTURE_32BITF | VertexType::COLOR_8888 | VertexType::VERTEX_32BITF | VertexType::TRANSFORM_3D,
+                VertexType::TEXTURE_32BITF
+                    | VertexType::COLOR_8888
+                    | VertexType::VERTEX_32BITF
+                    | VertexType::TRANSFORM_3D,
                 2,
                 ptr::null_mut(),
                 &vertices as *const Align16<_> as *const _,
-            );	
+            );
             sys::sceGuFinish();
             sys::sceGuSync(GuSyncMode::Finish, GuSyncBehavior::Wait);
-
         }
     }
 
@@ -110,23 +124,23 @@ impl<'a, T> Sprite<'a, T> where T: AsRef<[u8]> {
         (self.x, self.y)
     }
 
-    pub fn set_rotation_radians(&mut self, radians: f32) { 
+    pub fn set_rotation_radians(&mut self, radians: f32) {
         self.rotation_radians = radians;
     }
 
-    pub fn get_rotation_radians(&mut self) -> f32 { 
+    pub fn get_rotation_radians(&mut self) -> f32 {
         self.rotation_radians
     }
 
-    pub fn set_rotation_degrees(&mut self, degrees: f32) { 
+    pub fn set_rotation_degrees(&mut self, degrees: f32) {
         self.rotation_radians = degrees * (PI / 180.0);
     }
 
-    pub fn get_rotation_degrees(&mut self) -> f32 { 
+    pub fn get_rotation_degrees(&mut self) -> f32 {
         self.rotation_radians * (180.0 / PI)
     }
 
-    pub fn set_scale(&mut self, scale: f32) { 
+    pub fn set_scale(&mut self, scale: f32) {
         self.scale = scale;
     }
 }
--- examples/tetris/src/tetromino.rs
+++ examples/tetris/src/tetromino.rs
@@ -1,4 +1,4 @@
-use crate::sprite::{Sprite, self};
+use crate::sprite::{self, Sprite};
 use crate::BLOCK;
 use crate::BLOCK_SIZE;
 
@@ -6,7 +6,7 @@ pub struct Tetromino {
     x: i32,
     y: i32,
     color: u32,
-    block_locs: [(i32, i32); 4]
+    block_locs: [(i32, i32); 4],
 }
 
 impl Tetromino {
@@ -15,12 +15,7 @@ impl Tetromino {
             x: 0,
             y: 0,
             color: 0xff00ffff,
-            block_locs: [
-                (0, 1),
-                (1, 1),
-                (0, 0),
-                (1, 0),
-            ]
+            block_locs: [(0, 1), (1, 1), (0, 0), (1, 0)],
         }
     }
 
@@ -29,12 +24,7 @@ impl Tetromino {
             x: 0,
             y: 0,
             color: 0xffffff00,
-            block_locs: [
-                (0, 0),
-                (0, 1),
-                (0, 2),
-                (0, -1),
-            ]
+            block_locs: [(0, 0), (0, 1), (0, 2), (0, -1)],
         }
     }
 
@@ -43,12 +33,7 @@ impl Tetromino {
             x: 0,
             y: 0,
             color: 0xff0000ff,
-            block_locs: [
-                (0, 1),
-                (-1, 1),
-                (0, 0),
-                (1, 0),
-            ]
+            block_locs: [(0, 1), (-1, 1), (0, 0), (1, 0)],
         }
     }
 
@@ -57,12 +42,7 @@ impl Tetromino {
             x: 0,
             y: 0,
             color: 0xff00ff00,
-            block_locs: [
-                (0, 0),
-                (0, 1),
-                (-1, 0),
-                (1, 1),
-            ]
+            block_locs: [(0, 0), (0, 1), (-1, 0), (1, 1)],
         }
     }
 
@@ -71,12 +51,7 @@ impl Tetromino {
             x: 0,
             y: 0,
             color: 0xff008cff,
-            block_locs: [
-                (0, 1),
-                (0, 0),
-                (0, -1),
-                (-1, -1),
-            ]
+            block_locs: [(0, 1), (0, 0), (0, -1), (-1, -1)],
         }
     }
 
@@ -85,12 +60,7 @@ impl Tetromino {
             x: 0,
             y: 0,
             color: 0xffff00ff,
-            block_locs: [
-                (0, 1),
-                (0, 0),
-                (0, -1),
-                (1, -1),
-            ]
+            block_locs: [(0, 1), (0, 0), (0, -1), (1, -1)],
         }
     }
 
@@ -99,33 +69,58 @@ impl Tetromino {
             x: 0,
             y: 0,
             color: 0xffff0000,
-            block_locs: [
-                (1, 0),
-                (0, 0),
-                (-1, 0),
-                (0, -1),
-            ]
+            block_locs: [(1, 0), (0, 0), (-1, 0), (0, -1)],
         }
     }
 
-    pub fn as_sprites<'a>(&self) -> [Sprite<'a, [u8; BLOCK_SIZE as usize * BLOCK_SIZE as usize * 4]>; 4] {
+    pub fn as_sprites<'a>(
+        &self,
+    ) -> [Sprite<'a, [u8; BLOCK_SIZE as usize * BLOCK_SIZE as usize * 4]>; 4] {
         [
-            Sprite::new(&BLOCK.0, self.color, self.block_locs[0].0*BLOCK_SIZE as i32+self.x*BLOCK_SIZE as i32, self.block_locs[0].1*BLOCK_SIZE as i32+self.y*BLOCK_SIZE as i32, BLOCK_SIZE, BLOCK_SIZE),
-            Sprite::new(&BLOCK.0, self.color, self.block_locs[1].0*BLOCK_SIZE as i32+self.x*BLOCK_SIZE as i32, self.block_locs[1].1*BLOCK_SIZE as i32+self.y*BLOCK_SIZE as i32, BLOCK_SIZE, BLOCK_SIZE),
-            Sprite::new(&BLOCK.0, self.color, self.block_locs[2].0*BLOCK_SIZE as i32+self.x*BLOCK_SIZE as i32, self.block_locs[2].1*BLOCK_SIZE as i32+self.y*BLOCK_SIZE as i32, BLOCK_SIZE, BLOCK_SIZE),
-            Sprite::new(&BLOCK.0, self.color, self.block_locs[3].0*BLOCK_SIZE as i32+self.x*BLOCK_SIZE as i32, self.block_locs[3].1*BLOCK_SIZE as i32+self.y*BLOCK_SIZE as i32, BLOCK_SIZE, BLOCK_SIZE),
+            Sprite::new(
+                &BLOCK.0,
+                self.color,
+                self.block_locs[0].0 * BLOCK_SIZE as i32 + self.x * BLOCK_SIZE as i32,
+                self.block_locs[0].1 * BLOCK_SIZE as i32 + self.y * BLOCK_SIZE as i32,
+                BLOCK_SIZE,
+                BLOCK_SIZE,
+            ),
+            Sprite::new(
+                &BLOCK.0,
+                self.color,
+                self.block_locs[1].0 * BLOCK_SIZE as i32 + self.x * BLOCK_SIZE as i32,
+                self.block_locs[1].1 * BLOCK_SIZE as i32 + self.y * BLOCK_SIZE as i32,
+                BLOCK_SIZE,
+                BLOCK_SIZE,
+            ),
+            Sprite::new(
+                &BLOCK.0,
+                self.color,
+                self.block_locs[2].0 * BLOCK_SIZE as i32 + self.x * BLOCK_SIZE as i32,
+                self.block_locs[2].1 * BLOCK_SIZE as i32 + self.y * BLOCK_SIZE as i32,
+                BLOCK_SIZE,
+                BLOCK_SIZE,
+            ),
+            Sprite::new(
+                &BLOCK.0,
+                self.color,
+                self.block_locs[3].0 * BLOCK_SIZE as i32 + self.x * BLOCK_SIZE as i32,
+                self.block_locs[3].1 * BLOCK_SIZE as i32 + self.y * BLOCK_SIZE as i32,
+                BLOCK_SIZE,
+                BLOCK_SIZE,
+            ),
         ]
     }
 
     pub fn as_vertices(&self) -> [sprite::Vertex; 8] {
         let mut output = [sprite::Vertex::default(); 8];
         let mut output_pos = 0;
-        self.as_sprites()
-        .iter()
-        .for_each(|s| s.as_vertices().iter().for_each(|v| {
-            output[output_pos] = *v;
-            output_pos += 1
-        }));
+        self.as_sprites().iter().for_each(|s| {
+            s.as_vertices().iter().for_each(|v| {
+                output[output_pos] = *v;
+                output_pos += 1
+            })
+        });
         output
     }
 
@@ -140,14 +135,13 @@ impl Tetromino {
 
     pub fn rotate_ccw(&mut self) {
         for i in 0..4 {
-           self.block_locs[i] = (self.block_locs[i].1, self.block_locs[i].0);
+            self.block_locs[i] = (self.block_locs[i].1, self.block_locs[i].0);
         }
     }
 
     pub fn rotate_cw(&mut self) {
         for i in 0..4 {
-           self.block_locs[i] = (0-self.block_locs[i].1, self.block_locs[i].0);
+            self.block_locs[i] = (0 - self.block_locs[i].1, self.block_locs[i].0);
         }
     }
 }
-
